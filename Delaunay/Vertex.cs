834f5a6c9ec1ffee65f9a7b4757d766ba4650203
KotioNolik <fedorfomin200612@gmail.com>
2025-01-18 01:01:27 +0300

    True fin

--- Delaunay/Vertex.cs
+++ Delaunay/Vertex.cs
@@ -1,14 +1,4 @@
-﻿///////////////////////////////////////////////////////////////////////////////
-//
-//  Vertex.cs
-//
-//  By Philip R. Braica (HoshiKata@aol.com, VeryMadSci@gmail.com)
-//
-//  Distributed under the The Code Project Open License (CPOL)
-//  http://www.codeproject.com/info/cpol10.aspx
-///////////////////////////////////////////////////////////////////////////////
-
-// Namespace
+﻿
 namespace gg.Mesh
 {
     using System;
@@ -16,14 +6,8 @@ namespace gg.Mesh
     using System.Linq;
     using System.Text;
 
-    /// <summary>
-    /// Vertex class
-    /// </summary>
     public class Vertex
     {
-        /// <summary>
-        /// Vertex default constructor.
-        /// </summary>
         public Vertex()
         {
             X = 0;
@@ -31,12 +15,6 @@ namespace gg.Mesh
             Z = 0;
         }
 
-        /// <summary>
-        /// Vertex constructor.
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="z"></param>
         public Vertex(float x, float y, float z)
         {
             X = x;
@@ -44,31 +22,14 @@ namespace gg.Mesh
             Z = z;
         }
 
-        /// <summary>
-        /// X coordinate.
-        /// </summary>
         public float X { get; set; }
 
-        /// <summary>
-        /// Y coordinate.
-        /// </summary>
         public float Y { get; set; }
 
-        /// <summary>
-        /// Z coordinate.
-        /// </summary>
         public float Z { get; set; }
 
-        /// <summary>
-        /// Index of this vertex.
-        /// </summary>
         public int Index { get; set; }
 
-        /// <summary>
-        /// Delta distance squared between this and other vertex t
-        /// </summary>
-        /// <param name="t"></param>
-        /// <returns></returns>
         public float DeltaSquaredXY(Vertex t)
         {
             float dx = (X - t.X);
@@ -76,11 +37,6 @@ namespace gg.Mesh
             return (dx * dx) + (dy * dy);
         }
 
-        /// <summary>
-        /// Delta distance squared between this and other vertex t
-        /// </summary>
-        /// <param name="t"></param>
-        /// <returns></returns>
         public float DeltaSquared(Vertex t)
         {
             float dx = (X - t.X);
@@ -89,31 +45,16 @@ namespace gg.Mesh
             return (dx * dx) + (dy * dy) + (dz * dz);
         }
 
-        /// <summary>
-        /// The distance between this and t.
-        /// </summary>
-        /// <param name="t"></param>
-        /// <returns></returns>
         public float DistanceXY(Vertex t)
         {
             return (float)System.Math.Sqrt(DeltaSquaredXY(t));
         }
 
-        /// <summary>
-        /// The distance between this and t.
-        /// </summary>
-        /// <param name="t"></param>
-        /// <returns></returns>
         public float Distance(Vertex t)
         {
             return (float)System.Math.Sqrt(DeltaSquared(t));
         }
 
-        /// <summary>
-        /// Is this rectangle within the region.
-        /// </summary>
-        /// <param name="region"></param>
-        /// <returns></returns>
         public bool InsideXY(System.Drawing.RectangleF region)
         {
             if (X < region.Left) return false;
@@ -123,10 +64,6 @@ namespace gg.Mesh
             return true;
         }
 
-        /// <summary>
-        /// To string.
-        /// </summary>
-        /// <returns></returns>
         public override string ToString()
         {
             return X + ", " + Y;
